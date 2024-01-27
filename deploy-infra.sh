88c37bde4f45ea6081080bfdaee39c0d568a4782
Michael Gamov <nofater@gmail.com>
2024-01-27 19:25:53 +0100

    Add codebuild / codedeploy specs

---
+++ deploy-infra.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+STACK_NAME=awsbootstrap 
+REGION=eu-central-1
+CLI_PROFILE=awsbootstrap
+
+EC2_INSTANCE_TYPE=t2.micro 
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+# Deploy static resources
+aws cloudformation deploy \
+  --region $REGION
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
