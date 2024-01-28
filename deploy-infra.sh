18a23f6dffe9f99268c8abb826e4dd0385fea072
Michael Gamov <nofater@gmail.com>
2024-01-28 14:31:54 +0100

    Add rollout-on-commit deployment pipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -9,9 +9,15 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 # Deploy static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
-  --region $REGION
+  --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME-setup \
   --template-file setup.yml \
@@ -28,4 +34,16 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET 
+
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
