808a854cc87957e3808eb53321e5f7bb53ded9c1
Michael Gamov <nofater@gmail.com>
2024-01-28 19:05:16 +0100

    Extract Staging substack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -7,7 +7,8 @@ CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro 
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID" 
 
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
@@ -23,7 +24,25 @@ aws cloudformation deploy \
   --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET 
+
+# Package up CloudFormation templates into an S3 bucket
+echo -e "\n\n=========== Packaging main.yml ==========="
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+  exit 1
+fi
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -31,7 +50,7 @@ aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file ./cfn_output/main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
@@ -40,11 +59,11 @@ aws cloudformation deploy \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET 
-
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
     --query "Exports[].Value" 
 fi
+
